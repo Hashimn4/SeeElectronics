7c6fbd1a15dd011ae062e0052307b1b5fd3e66ed
Hashim Nadeem <hashimnadeem04@gmail.com>
2022-07-05 19:21:06 -0400

    Commiting

--- src/components/apollo-city.jsx
+++ src/components/apollo-city.jsx
@@ -1,28 +1,11 @@
 import React from 'react'
 import image1 from '../images/apollo_city.jpeg'
-const title1 = "Apollo City";
-const price = 79.99;
+import Card from './card'
+
 const apollo_city = () => {
   return (
     <>
-    <div className="scooter_image">
-            <img src={image1} alt={title1} />
-          </div>
-          <div className="scooter_title">
-            {title1}
-          </div>
-            <div className="scooter_price">
-                <h3>$79.99</h3>
-            </div>
-          <button className='buy-button snipcart-add-item'
-            data-item-id="apollocity"
-            data-item-image={image1}
-            data-item-name={title1}
-            data-item-price= {price}
-            data-item-url="/"
-          >
-            Add
-            </button>
+      <Card title="Apollo City" pic={image1} price="$949.99"/>
     </>
     
   )
--- src/components/electric_scooters.jsx
+++ src/components/electric_scooters.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './electric_scooters.css'
 import image1 from '../images/apollo_city.jpeg'
 import image2 from '../images/glion_dolly.jpeg'
@@ -8,12 +9,13 @@ import image5 from '../images/swagtron_swagger_5_elite.jpeg'
 import image6 from '../images/unagi_model_one.jpeg'
 import Card from './card'
 
+
 const electric_scooters = () => {
   return (
     
         <div className="container electricScooters_container">
     
-          <Card pic={image1} title = "Apollo City" price = "$949.99"/>
+          <Link to="/SeeElectronics/electricscooters/apollocity"><Card pic={image1} title = "Apollo City" price = "$949.99"/></Link>
           <Card pic={image2} title = "Glion Dolly" price = "$599.99"/>
           <Card pic={image3} title = "Razor e100" price = "$299.99"/>
           <Card pic={image4} title = "Segway Ninebot Kickstarter Max" price = "$999.99"/>
